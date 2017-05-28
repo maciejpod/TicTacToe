ff21be8bb3aeeb1ad46d1d14d08364cb93dd4562
m.podolanski <m.podolanski@gmail.com>
2017-05-28 15:18:12 +0200

    Added field select tests

--- app/ticTacToe.js
+++ app/ticTacToe.js
@@ -1,3 +1,39 @@
 /**
  * Created by maciej on 28.05.17.
  */
+
+var board = {
+    fields: ['', '', '', '', '', '', '', '', ''],
+    currentPlayer: 'x',
+};
+
+function play(x, y) {
+
+    if(x < 0) {
+        throw new Error('X value cannot be negative!')
+    }
+
+    if(y < 0) {
+        throw new Error('Y value cannot be negative!')
+    }
+
+    if(y < 1 || y > 3) {
+        throw new Error('Y value is outside of the board!')
+    }
+
+    if(x < 1 || x > 3) {
+        throw new Error('X value is outside of the board!')
+    }
+
+    if(board[(x-1) * 3 + (y-1)] !== '') {
+        throw new Error('Selected field is occupied by enemy!')
+    }
+
+    board[(x-1) * 3 + (y-1)] = 'x';
+}
+
+
+module.exports = {
+    play: play,
+    board: board
+}
--- tests/indexTest.spec.js
+++ tests/indexTest.spec.js
@@ -6,13 +6,26 @@
 var expect = require('chai').expect;
 var app = require('../app/ticTacToe');
 
-describe('x', function ()
-{
-    describe('x', function ()
-    {
-        it('x', function ()
-        {
+describe('Select field tests', function () {
 
-        });
+    it('When y is outside of the box should throw an exception', function () {
+        expect(app.play.bind(this, 2, 5)).to.throw('Y value is outside of the board!');
+    });
+
+    it('When x is outside of the box should throw an exception', function () {
+        expect(app.play.bind(this, 10, 1)).to.throw('X value is outside of the board!');
+    });
+
+    it('When x has negative value should throw an exception', function () {
+        expect(app.play.bind(this, -1, 10)).to.throw('X value cannot be negative!');
+    });
+
+    it('When y has negative value should throw an exception', function () {
+        expect(app.play.bind(this, 2, -10)).to.throw('Y value cannot be negative!');
+    });
+
+    it('When field is occupied should throw an exception', function () {
+        app.play.bind(1, 1);
+        expect(app.play.bind(this, 1, 1)).to.throw('Selected field is occupied by enemy!');
     });
 });
