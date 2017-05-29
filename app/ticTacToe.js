7bff05c1ed6e71147f4126749016822719792f21
m.podolanski <m.podolanski@gmail.com>
2017-05-29 21:50:56 +0200

    Refactor

--- app/ticTacToe.js
+++ app/ticTacToe.js
@@ -8,31 +8,29 @@ class TicTacToe {
         this.currentPlayer = 'x';
     }
 
+
+    checkAxis(value, axis) {
+        if (value < 0) {
+            throw new Error(axis + ' value cannot be negative!');
+        }
+
+        if (value < 1 || value > 3) {
+            throw new Error(axis + ' value is outside of the board!')
+        }
+    };
+
     play(x, y) {
+        this.checkAxis(x, 'X');
+        this.checkAxis(y, 'Y');
+        this.selectField(x, y);
+        this.nextPlayer();
+    }
 
-        if (x < 0) {
-            throw new Error('X value cannot be negative!')
-        }
-
-        if (y < 0) {
-            throw new Error('Y value cannot be negative!')
-        }
-
-        if (y < 1 || y > 3) {
-            throw new Error('Y value is outside of the board!')
-        }
-
-        if (x < 1 || x > 3) {
-            throw new Error('X value is outside of the board!')
-        }
-
+    selectField(x , y) {
         if (this.fields[(x - 1) * 3 + (y - 1)] !== '') {
             throw new Error('Selected field is occupied by enemy!')
         }
-
         this.fields[(x - 1) * 3 + (y - 1)] = this.fields.currentPlayer;
-
-        this.nextPlayer();
     }
 
     nextPlayer() {
