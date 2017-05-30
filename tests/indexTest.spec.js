0f2e071c6c155d06faa631b79eb81f6d5e9ce752
Mateusz Mikula <mati865@gmail.com>
2017-05-31 00:00:06 +0200

    Refactor and lint

--- tests/indexTest.spec.js
+++ tests/indexTest.spec.js
@@ -1,13 +1,14 @@
+/* eslint-disable max-len */
 /**
  * Created by maciej && mateusz on 28.05.17.
  */
 'use strict';
 
-var expect = require('chai').expect;
-var TicTacToe = require('../app/ticTacToe');
+let expect = require('chai').expect;
+let TicTacToe = require('../lib/ticTacToe');
 
 describe('Game conditions', () => {
-    var instance;
+    let instance;
 
     beforeEach(() => {
         instance = new TicTacToe();
@@ -18,45 +19,44 @@ describe('Game conditions', () => {
     });
 
     it('When x player won the game should return "x won the game!(horizontal)', () => {
-        instance.play(1, 1); instance.play(1 ,2);
-        instance.play(2 ,1); instance.play(1 ,3);
-        expect(instance.play(3 ,1)).to.eql('x won the game!');
+        instance.play(1, 1); instance.play(1, 2);
+        instance.play(2, 1); instance.play(1, 3);
+        expect(instance.play(3, 1)).to.eql('x won the game!');
     });
 
     it('When o player won the game should return "o won the game!(horizontal)', () => {
-        instance.play(1 ,3); instance.play(1, 1);
-        instance.play(1 ,2); instance.play(2 ,1); instance.play(3 ,3);
-        expect(instance.play(3 ,1)).to.eql('o won the game!');
+        instance.play(1, 3); instance.play(1, 1);
+        instance.play(1, 2); instance.play(2, 1); instance.play(3, 3);
+        expect(instance.play(3, 1)).to.eql('o won the game!');
     });
 
     it('When x player won the game should return "x won the game!(vertical)', () => {
-        instance.play(1, 1); instance.play(2 ,1);
-        instance.play(1 ,2); instance.play(3 ,1);
-        expect(instance.play(1 ,3)).to.eql('x won the game!');
+        instance.play(1, 1); instance.play(2, 1);
+        instance.play(1, 2); instance.play(3, 1);
+        expect(instance.play(1, 3)).to.eql('x won the game!');
     });
 
     it('When o player won the game should return "o won the game!(vertical)', () => {
-        instance.play(3 ,1); instance.play(1, 1);
-        instance.play(2 ,1); instance.play(1 ,2); instance.play(3 ,3);
-        expect(instance.play(1 ,3)).to.eql('o won the game!');
+        instance.play(3, 1); instance.play(1, 1);
+        instance.play(2, 1); instance.play(1, 2); instance.play(3, 3);
+        expect(instance.play(1, 3)).to.eql('o won the game!');
     });
 
     it('When o player won the game should return "o won the game!(cross)', () => {
-        instance.play(1 ,2); instance.play(1, 1);
-        instance.play(2 ,1); instance.play(2 ,2); instance.play(3 ,2);
-        expect(instance.play(3 ,3)).to.eql('o won the game!');
+        instance.play(1, 2); instance.play(1, 1);
+        instance.play(2, 1); instance.play(2, 2); instance.play(3, 2);
+        expect(instance.play(3, 3)).to.eql('o won the game!');
     });
 
     it('When x player won the game should return "x won the game!(cross)', () => {
-        instance.play(1, 3);  instance.play(1, 2);
+        instance.play(1, 3); instance.play(1, 2);
         instance.play(2, 2); instance.play(2, 1);
-        expect(instance.play(3 ,1)).to.eql('x won the game!');
+        expect(instance.play(3, 1)).to.eql('x won the game!');
     });
 });
 
 describe('Select field tests', () => {
-
-    var instance;
+    let instance;
 
     beforeEach(() => {
         instance = new TicTacToe();
@@ -64,32 +64,32 @@ describe('Select field tests', () => {
 
     it('When y is outside of the box should throw an exception', () => {
         expect(() => {
-            instance.play(2, 5)
+            instance.play(2, 5);
         }).to.throw('Y value is outside of the board!');
     });
 
     it('When x is outside of the box should throw an exception', () => {
         expect(() => {
-            instance.play(10, 1)
+            instance.play(10, 1);
         }).to.throw('X value is outside of the board!');
     });
 
     it('When x has negative value should throw an exception', () => {
         expect(() => {
-            instance.play(-1, 10)
+            instance.play(-1, 10);
         }).to.throw('X value cannot be negative!');
     });
 
     it('When y has negative value should throw an exception', () => {
         expect(() => {
-            instance.play(2, -10)
+            instance.play(2, -10);
         }).to.throw('Y value cannot be negative!');
     });
 
     it('When field is occupied should throw an exception', () => {
         instance.play(1, 1);
         expect(() => {
-            instance.play(1, 1)
+            instance.play(1, 1);
         }).to.throw('Selected field is occupied by enemy!');
     });
 
