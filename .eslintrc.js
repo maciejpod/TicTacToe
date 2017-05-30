88447df8fd89b90304b020aeef8be6e4e11dec24
Mateusz Mikula <mati865@gmail.com>
2017-05-30 23:58:19 +0200

    Add ESlint with rules

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "extends": ["eslint:recommended", "google"],
+    "env": {
+        "es6": true,
+        "mocha": true,
+        "node": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 7,
+        "sourceType": "module"
+    },
+    "rules": {
+        "require-jsdoc": 0
+    }
+};
+
